afe614cadf8b10c96d2ba97f3c0fb36f8ffbe4e1
mrunalpatel2027 <133611286+mrunalpatel2027@users.noreply.github.com>
2023-07-12 15:51:00 -0400

    on script

--- create_db.py
+++ create_db.py
@@ -7,6 +7,8 @@ Usage:
  python create_db.py
 """
 import os
+import sqlite3
+from datetime import datetime
 
 # Determine the path of the database
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,13 +21,73 @@ def main():
 def create_people_table():
     """Creates the people table in the database"""
     # TODO: Create function body 
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    create_ppl_tbl_query = """
+        CREATE TABLE IF NOT EXISTS people
+        (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        address TEXT NOT NULL,
+        city TEXT NOT NULL,
+        province TEXT NOT NULL,
+        bio TEXT,
+        age INTEGER,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL
+        );
+        """
+    cur.execute(create_ppl_tbl_query)
+    con.commit()
+    con.close()
     # Hint: See example code in lab instructions entitled "Creating a Table"
     return
 
 def populate_people_table():
     """Populates the people table with 200 fake people"""
     # TODO: Create function body
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    add_person_query = """
+            INSERT INTO people
+            (
+            name,
+            email,
+            address,
+            city,
+            province,
+            bio,
+            age,
+            created_at,
+            updated_at
+            )
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+            """
+
+    new_person = ('Bob Loblaw',
+            'bob.loblaw@whatever.net',
+            '123 Fake St.',
+            'Fakesville',
+            'Fake Edward Island',
+            'Enjoys making funny sounds when talking.',
+            46,
+            datetime.now(),
+            datetime.now())
+
+    cur.execute(add_person_query, new_person)
+    con.commit()
+    con.close()
+
+
     # Hint: See example code in lab instructions entitled "Inserting Data into a Table"
+    from faker import Faker
+
+    fake = Faker("en_CA")
+    for _ in range(10):
+        province = fake.administrative_unit()
+        population = fake.random_int(min=900000, max=100000000)
+        print(f'The population of {province} is {population}.')
     # Hint: See example code in lab instructions entitled "Working with Faker"
     return
 
--- old_people.py
+++ old_people.py
@@ -8,6 +8,8 @@ Usage:
 """
 import os
 from create_db import db_path, script_dir
+import sqlite3
+from pprint import pprint
 
 def main():
     old_people_list = get_old_people()
@@ -23,6 +25,14 @@ def get_old_people():
         list: (name, age) of old people 
     """
     # TODO: Create function body
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM people')
+    all_people = cur.fetchall()
+    pprint(all_people)
+    con.commit()
+    con.close()
+
     # Hint: See example code in lab instructions entitled "Getting People Data from the Database"
     return
 
@@ -33,6 +43,8 @@ def print_name_and_age(name_and_age_list):
         name_and_age_list (list): (name, age) of people
     """
     # TODO: Create function body
+
+
     # Hint: Use a for loop to iterate the list of tuples to print a sentence for each old person
     return
 
@@ -44,6 +56,8 @@ def save_name_and_age_to_csv(name_and_age_list, csv_path):
         csv_path (str): Path of CSV file
     """
     # TODO: Create function body
+    
+
     # Hint: In Lab 3, we converted a list of tuples into a pandas DataFrame and saved it to a CSV file
     return
 
